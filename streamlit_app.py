75e8458e9838a3419e4e0ff6eeda55dc6bf714e3
ShinmyoHanHIM <151005976+ShinmyoHanHIM@users.noreply.github.com>
2023-12-05 01:12:54 +0000

    Changes to be committed:

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,15 @@
 import streamlit as st
 from streamlit_extras.add_vertical_space import add_vertical_space
+
+# Sidebar contents
+with st.sidebar:
+    st.title('LLM Chat app')
+    st.markdown('''
+                ## About
+                This app is an llm-powered chatbot built using:
+                - [streamlit](https://streamlit.com/)
+                - [Langchain](https://python.langchain.com/)
+                - [OpenAI](https://platform.openai.com/docs/models) LLM model
+                ''')
+    add_vertical_space(5)
+    st.write('First streamlit')
